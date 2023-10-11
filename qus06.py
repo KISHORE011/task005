0e439f6a430587370b88ac35f37766ba11db6f89
KISHORE011 <kishore011@gmail.com>
2023-10-10 16:26:23 -0800

    qus06

---
+++ qus06.py
@@ -0,0 +1,26 @@
+def longest_common_substring(str1, str2):
+    m = len(str1)
+    n = len(str2)
+    
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
+    
+    max_length = 0
+    end_index = 0
+    
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if str1[i - 1] == str2[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1] + 1
+                if dp[i][j] > max_length:
+                    max_length = dp[i][j]
+                    end_index = i - 1  
+    
+    longest_substring = str1[end_index - max_length + 1:end_index + 1]
+    
+    return longest_substring
+
+str1 = "GuviGeeksNetworkPrivateLimited"
+str2 = "KISHORE"
+result = longest_common_substring(str1, str2)
+print("Longest Common Substring:", result)
+
