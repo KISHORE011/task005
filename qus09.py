6eab2d0338c6b66fb0f4e070fd5ce0e1009bfaef
KISHORE011 <kishore011@gmail.com>
2023-10-10 16:34:33 -0800

    qus09

---
+++ qus09.py
@@ -0,0 +1,9 @@
+def count_words(input_string):
+    words = input_string.split()
+    
+    return len(words)
+
+input_string = "This is a sample sentence."
+word_count = count_words(input_string)
+print("Number of words:", word_count)
+
