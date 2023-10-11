8a3c776a945cf8c582992b5ea6301206797fdd80
KISHORE011 <kishore011@gmail.com>
2023-10-10 16:02:42 -0800

    code

---
+++ qus01.py
@@ -0,0 +1,30 @@
+input_string = "Guvi Geeks Network Private Limited"
+
+input_string = input_string.lower()
+
+count_a = 0
+count_e = 0
+count_i = 0
+count_o = 0
+count_u = 0
+
+for char in input_string:
+    if char == 'a':
+        count_a += 1
+    elif char == 'e':
+        count_e += 1
+    elif char == 'i':
+        count_i += 1
+    elif char == 'o':
+        count_o += 1
+    elif char == 'u':
+        count_u += 1
+
+total_vowels = count_a + count_e + count_i + count_o + count_u
+
+print("Total vowels:", total_vowels)
+print("Count of 'A':", count_a)
+print("Count of 'E':", count_e)
+print("Count of 'I':", count_i)
+print("Count of 'O':", count_o)
+print("Count of 'U':", count_u)
